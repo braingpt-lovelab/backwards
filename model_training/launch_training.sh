2f0f55cf70643309aa5d6b2fcf6de3eddb9ceb8c
Ubuntu <ken@braingptbenchmark3.r3hnxk2l5ypunf4zeol2qq0dca.gvxx.internal.cloudapp.net>
2024-10-28 14:50:54 +0000

    updated training code to support seed

--- model_training/launch_training.sh
+++ model_training/launch_training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the JSON file and expdir
-args_json="configs/gpt2-large_scratch_neuro_tokenizer_backwards.json"
+args_json="configs/gpt2_scratch_neuro_tokenizer_seed2.json"
 
 expdir=$(python3 -c "import json; import sys; d = json.load(open('$args_json')); sys.stdout.write(d['outputdir'])")
 mkdir -p $expdir
@@ -10,4 +10,11 @@ mkdir -p $expdir
 key_value_pairs=$(python3 -c "import json; import sys; d = json.load(open('$args_json')); sys.stdout.write(' '.join([f'--{key} {value}' for key, value in d.items()]))")
 
 # Launch the Python script with the dynamically created arguments
-accelerate launch --config_file accel_config.yaml train_backwards.py $key_value_pairs
+accelerate launch --config_file accel_config.yaml train.py $key_value_pairs
+
+# Run another training job with a different seed
+args_json="configs/gpt2_scratch_neuro_tokenizer_seed3.json"
+expdir=$(python3 -c "import json; import sys; d = json.load(open('$args_json')); sys.stdout.write(d['outputdir'])")
+mkdir -p $expdir
+key_value_pairs=$(python3 -c "import json; import sys; d = json.load(open('$args_json')); sys.stdout.write(' '.join([f'--{key} {value}' for key, value in d.items()]))")
+accelerate launch --config_file accel_config.yaml train.py $key_value_pairs
