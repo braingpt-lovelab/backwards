a9aea8fe6d588aadfb167b5affeb1a9efa0b6046
Ken <ken.xiaoliang@gmail.com>
2024-11-11 11:12:17 +0000

    update to plot all figs in paper

--- analyses/plot_paper_figs.sh
+++ analyses/plot_paper_figs.sh
@@ -1,11 +1,11 @@
-# Fig 1
+# Fig. 1
 python plot_model_vs_human.py
 
-# Fig 2
+# Fig. 2
 python plot_ppl_val_and_test.py
 
-# Fig 3
+# Fig. 3
 python plot_error_correlation_model_vs_human.py
 
-# Appendix
+# Fig. S1
 python plot_token_analyses.py
