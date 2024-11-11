ac5643f6a66e4f1835404734f1c027bef7d387ac
Ken <ken.xiaoliang@gmail.com>
2024-11-11 11:09:24 +0000

    rename script names to be consistent for plotting

---
+++ analyses/plot_paper_figs.sh
@@ -0,0 +1,11 @@
+# Fig 1
+python plot_model_vs_human.py
+
+# Fig 2
+python plot_ppl_val_and_test.py
+
+# Fig 3
+python plot_error_correlation_model_vs_human.py
+
+# Appendix
+python plot_token_analyses.py
