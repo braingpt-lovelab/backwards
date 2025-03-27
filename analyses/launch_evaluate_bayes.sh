8ec28e3ba283a84e0dc5553a61174bd775a988f4
Ubuntu <ken@braingptbenchmark3.r3hnxk2l5ypunf4zeol2qq0dca.gvxx.internal.cloudapp.net>
2025-03-27 19:02:47 +0000

    logged notes (resolved): `get_ppl_final_bayes.py` produced different average val ppl compared to log on wandb: 1) get_ppl_final_bayes does average per sequence than average but wandb does total_loss/total_tokens; 2) single GPU and multi-GPU produce slightly different val ppl due to DPP pads last batch to full `batch_size` with duplication

---
+++ analyses/launch_evaluate_bayes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define the JSON file and expdir
+args_json="configs/gpt2_scratch_neuro_tokenizer_bayes_fwd.json"
+
+expdir=$(python3 -c "import json; import sys; d = json.load(open('$args_json')); sys.stdout.write(d['outputdir'])")
+mkdir -p $expdir
+
+# Extract the JSON contents into Bash-friendly key-value pairs
+key_value_pairs=$(python3 -c "import json; import sys; d = json.load(open('$args_json')); sys.stdout.write(' '.join([f'--{key} {value}' for key, value in d.items()]))")
+
+# Launch the Python script with the dynamically created arguments
+accelerate launch --config_file accel_config.yaml evaluate_bayes.py $key_value_pairs
