de0257e5b58aeae0c823621525e3db6bb13b1333
Ken <ken.xiaoliang@gmail.com>
2025-05-11 21:43:54 +0100

    improved prints

--- analyses/plot_x_models_x_seeds_x_directions_diffs_bayes.py
+++ analyses/plot_x_models_x_seeds_x_directions_diffs_bayes.py
@@ -79,12 +79,11 @@ comparisons = {
 
 def compute_stats(model1_ppls, model2_ppls):
     t_stat, p_val = stats.ttest_rel(model1_ppls, model2_ppls)
-    pearson_r = np.corrcoef(model1_ppls, model2_ppls)[0, 1]
+    pearson_r, pearson_r_p_val = stats.pearsonr(model1_ppls, model2_ppls)
     cohen_d = (np.mean(model1_ppls) - np.mean(model2_ppls)) / np.sqrt(
         np.var(model1_ppls - model2_ppls, ddof=1)
     )
-    pearson_r = np.corrcoef(model1_ppls, model2_ppls)[0, 1]
-    return t_stat, p_val, pearson_r, cohen_d
+    return t_stat, p_val, pearson_r, pearson_r_p_val, cohen_d
 
 
 def x_seeds_diffs(comparison, data_type="validation"):
@@ -100,6 +99,7 @@ def x_seeds_diffs(comparison, data_type="validation"):
                 "t_stat": [],
                 "p_val": [],
                 "pearson_r": [],
+                "pearson_r_p_val": [],
                 "cohen_d": []
             }
             for i in range(len(model_names)):
@@ -117,28 +117,32 @@ def x_seeds_diffs(comparison, data_type="validation"):
                     )
 
                     # Compute stats
-                    t_stat, p_val, pearson_r, cohen_d = compute_stats(model1_ppls, model2_ppls)
+                    t_stat, p_val, pearson_r, pearson_r_p_val, cohen_d = compute_stats(model1_ppls, model2_ppls)
                     per_direction_model_pairs_stats["t_stat"].append(t_stat)
                     per_direction_model_pairs_stats["p_val"].append(p_val)
                     per_direction_model_pairs_stats["pearson_r"].append(pearson_r)
+                    per_direction_model_pairs_stats["pearson_r_p_val"].append(pearson_r_p_val)
                     per_direction_model_pairs_stats["cohen_d"].append(cohen_d)
 
             # Compute average stats for the direction
             avg_t_stat = np.mean(per_direction_model_pairs_stats["t_stat"])
             avg_p_val = np.mean(per_direction_model_pairs_stats["p_val"])
             avg_pearson_r = np.mean(per_direction_model_pairs_stats["pearson_r"])
+            avg_pearson_r_p_val = np.mean(per_direction_model_pairs_stats["pearson_r_p_val"])
             avg_cohen_d = np.mean(per_direction_model_pairs_stats["cohen_d"])
             std_t_stat = np.std(per_direction_model_pairs_stats["t_stat"])
             std_p_val = np.std(per_direction_model_pairs_stats["p_val"])
             std_pearson_r = np.std(per_direction_model_pairs_stats["pearson_r"])
+            std_pearson_r_p_val = np.std(per_direction_model_pairs_stats["pearson_r_p_val"])
             std_cohen_d = np.std(per_direction_model_pairs_stats["cohen_d"])
 
             # print for per family and per direction
             print(f"    Average stats for {direction}:")
-            print(f"      t_stat: {avg_t_stat:.4f} ± {std_t_stat:.4f}")
-            print(f"      p_val: {avg_p_val:.4f} ± {std_p_val:.4f}")
-            print(f"      pearson_r: {avg_pearson_r:.4f} ± {std_pearson_r:.4f}")
-            print(f"      cohen_d: {avg_cohen_d:.4f} ± {std_cohen_d:.4f}")
+            print(f"      pearson_r: {avg_pearson_r:.3f} ± {std_pearson_r:.3f}")
+            print(f"      pearson_r_p_val: {avg_pearson_r_p_val:.3f} ± {std_pearson_r_p_val:.3f}")
+            print(f"      t_stat: {avg_t_stat:.3f} ± {std_t_stat:.3f}")
+            print(f"      p_val: {avg_p_val:.3f} ± {std_p_val:.3f}")
+            print(f"      cohen_d: {abs(avg_cohen_d):.3f} ± {std_cohen_d:.3f}")
 
                 
 def x_direction_diffs(comparison, data_type="validation"):
@@ -155,6 +159,7 @@ def x_direction_diffs(comparison, data_type="validation"):
                 "t_stat": [],
                 "p_val": [],
                 "pearson_r": [],
+                "pearson_r_p_val": [],
                 "cohen_d": []
             }
             for model_pair in model_pairs:
@@ -170,28 +175,32 @@ def x_direction_diffs(comparison, data_type="validation"):
                 )
                 
                 # Compute stats
-                t_stat, p_val, pearson_r, cohen_d = compute_stats(model1_ppls, model2_ppls)
+                t_stat, p_val, pearson_r, pearson_r_p_val, cohen_d = compute_stats(model1_ppls, model2_ppls)
                 per_direction_pair_model_pairs_stats["t_stat"].append(t_stat)
                 per_direction_pair_model_pairs_stats["p_val"].append(p_val)
                 per_direction_pair_model_pairs_stats["pearson_r"].append(pearson_r)
+                per_direction_pair_model_pairs_stats["pearson_r_p_val"].append(pearson_r_p_val)
                 per_direction_pair_model_pairs_stats["cohen_d"].append(cohen_d)
         
             # Compute average stats for the direction
             avg_t_stat = np.mean(per_direction_pair_model_pairs_stats["t_stat"])
             avg_p_val = np.mean(per_direction_pair_model_pairs_stats["p_val"])
             avg_pearson_r = np.mean(per_direction_pair_model_pairs_stats["pearson_r"])
+            avg_pearson_r_p_val = np.mean(per_direction_pair_model_pairs_stats["pearson_r_p_val"])
             avg_cohen_d = np.mean(per_direction_pair_model_pairs_stats["cohen_d"])
             std_t_stat = np.std(per_direction_pair_model_pairs_stats["t_stat"])
             std_p_val = np.std(per_direction_pair_model_pairs_stats["p_val"])
             std_pearson_r = np.std(per_direction_pair_model_pairs_stats["pearson_r"])
+            std_pearson_r_p_val = np.std(per_direction_pair_model_pairs_stats["pearson_r_p_val"])
             std_cohen_d = np.std(per_direction_pair_model_pairs_stats["cohen_d"])
 
             # print for per family and per direction pair
             print(f"    Average stats for {direction_pair}:")
-            print(f"      t_stat: {avg_t_stat:.4f} ± {std_t_stat:.4f}")
-            print(f"      p_val: {avg_p_val:.4f} ± {std_p_val:.4f}")
-            print(f"      pearson_r: {avg_pearson_r:.4f} ± {std_pearson_r:.4f}")
-            print(f"      cohen_d: {avg_cohen_d:.4f} ± {std_cohen_d:.4f}")
+            print(f"      pearson_r: {avg_pearson_r:.3f} ± {std_pearson_r:.3f}")
+            print(f"      pearson_r_p_val: {avg_pearson_r_p_val:.3f} ± {std_pearson_r_p_val:.3f}")
+            print(f"      t_stat: {avg_t_stat:.3f} ± {std_t_stat:.3f}")
+            print(f"      p_val: {avg_p_val:.3f} ± {std_p_val:.3f}")
+            print(f"      cohen_d: {abs(avg_cohen_d):.3f} ± {std_cohen_d:.3f}")
            
         
 def main():
